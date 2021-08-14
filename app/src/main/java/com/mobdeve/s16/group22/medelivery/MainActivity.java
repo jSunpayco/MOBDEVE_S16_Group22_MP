372894d2d71b4c04dd13f75ac4aba20355397553
Julian <julian_sunpacyo@dlsu.edu.ph>
2021-08-14 21:22:08 +0800

    in progress

--- app/src/main/java/com/mobdeve/s16/group22/medelivery/MainActivity.java
+++ app/src/main/java/com/mobdeve/s16/group22/medelivery/MainActivity.java
@@ -1,14 +1,69 @@
 package com.mobdeve.s16.group22.medelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText loginFNameET, loginLNameET, loginMailET, loginPasswordET;
+    Button loginBtn, noAccountBtn;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //this.loginFNameET = findViewById(R.id.loginFNameET);
+        //this.loginLNameET = findViewById(R.id.loginLNameET);
+        this.loginMailET = findViewById(R.id.loginMailET);
+        this.loginPasswordET = findViewById(R.id.loginPasswordET);
+        this.loginBtn = findViewById(R.id.loginBtn);
+        this.noAccountBtn = findViewById(R.id.noAccountBtn);
+
+        this.fAuth = FirebaseAuth.getInstance();
+
+        this.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String fname = loginFNameET.getText().toString().trim();
+                //String lname = loginLNameET.getText().toString().trim();
+                String email = loginMailET.getText().toString().trim();
+                String pass = loginPasswordET.getText().toString().trim();
+
+//                if(TextUtils.isEmpty(fname)){
+//                    registerFNameET.setError("Please input your first name.");
+//                    return;
+//                }
+
+//                if(TextUtils.isEmpty(lname)){
+//                    registerFNameET.setError("Please input your surname.");
+//                    return;
+//                }
+
+                if(TextUtils.isEmpty(email)){
+                    loginMailET.setError("Please input your email address.");
+                    return;
+                }
+
+                if(pass.length() < 8){
+                    loginPasswordET.setError("Please input your password.");
+                    return;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/mobdeve/s16/group22/medelivery/RegisterActivity.java
+++ app/src/main/java/com/mobdeve/s16/group22/medelivery/RegisterActivity.java
@@ -1,14 +1,86 @@
 package com.mobdeve.s16.group22.medelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText registerFNameET, registerLNameET, registerMailET, registerPasswordET;
+    Button registerBtn;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //this.registerFNameET = findViewById(R.id.registerFNameET);
+        //this.registerLNameET = findViewById(R.id.registerLNameET);
+        this.registerMailET = findViewById(R.id.registerMailET);
+        this.registerPasswordET = findViewById(R.id.registerPasswordET);
+        this.registerBtn = findViewById(R.id.registerBtn);
+
+        this.fAuth = FirebaseAuth.getInstance();
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+            finish();
+        }
+
+        this.registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String fname = registerFNameET.getText().toString().trim();
+                //String lname = registerLNameET.getText().toString().trim();
+                String email = registerMailET.getText().toString().trim();
+                String pass = registerPasswordET.getText().toString().trim();
+
+//                if(TextUtils.isEmpty(fname)){
+//                    registerFNameET.setError("Please input your first name.");
+//                    return;
+//                }
+
+//                if(TextUtils.isEmpty(lname)){
+//                    registerFNameET.setError("Please input your surname.");
+//                    return;
+//                }
+
+                if(TextUtils.isEmpty(email)){
+                    registerMailET.setError("Please input your email address.");
+                    return;
+                }
+
+                if(pass.length() < 8){
+                    registerPasswordET.setError("Please input a password with more than 8 characters.");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "User Registered", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
