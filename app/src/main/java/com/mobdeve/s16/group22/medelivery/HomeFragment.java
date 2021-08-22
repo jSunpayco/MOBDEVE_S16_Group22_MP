bd80bb039f1d7111a21445ca7a3325597d3b3152
AnjeloTing <anjelo1199@yahoo.com>
2021-08-23 01:37:38 +0800

    Created the onclick function for Item List in the homeFragment. Created the activity file for ItemListActivity. ToDo: ItemList getting the data from firebaseDB to display in the recyclerviewer. Set Onclick listeners for the 3 other buttons and create activity for each one.

--- app/src/main/java/com/mobdeve/s16/group22/medelivery/HomeFragment.java
+++ app/src/main/java/com/mobdeve/s16/group22/medelivery/HomeFragment.java
@@ -1,14 +1,22 @@
 package com.mobdeve.s16.group22.medelivery;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import static android.content.ContentValues.TAG;
 
 public class HomeFragment extends Fragment {
+    private ImageView list,about,cart,handbook;
+    Intent intent;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -17,7 +25,43 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        this.list = (ImageView) view.findViewById(R.id.listIb);
+        this.about = (ImageView) view.findViewById(R.id.aboutIb);
+        this.cart = (ImageView) view.findViewById(R.id.cartIb);
+        this.handbook = (ImageView) view.findViewById(R.id.handbookIb);
+
+        this.list.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked List");
+                Intent intent = new Intent(getActivity(), ItemListActivity.class);
+                ((MainActivity) getActivity()).startActivity(intent);
+            }
+        });
+//        this.about.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: clicked about");
+//                startActivity(intent);
+//            }
+//        });
+//        this.cart.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: clicked cart");
+//                startActivity(intent);
+//            }
+//        });
+//        this.handbook.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: clicked handbook");
+//                startActivity(intent);
+//            }
+//        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 }
---
+++ app/src/main/java/com/mobdeve/s16/group22/medelivery/ItemListActivity.java
@@ -0,0 +1,15 @@
+package com.mobdeve.s16.group22.medelivery;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.items_list_layout);
+    }
+}
