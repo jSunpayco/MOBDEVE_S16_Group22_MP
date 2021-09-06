9fa8306b54c4c367b76f3e69b800b70414b62ba1
Julian <julian_sunpacyo@dlsu.edu.ph>
2021-09-06 15:03:19 +0800

    Removed unnecessary files

--- app/src/main/java/com/mobdeve/s16/group22/medelivery/CartPriceInterface.java
+++
@@ -1,4 +0,0 @@
-package com.mobdeve.s16.group22.medelivery;
-
-public class CartPriceInterface {
-}
--- app/src/main/java/com/mobdeve/s16/group22/medelivery/OnItemsClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.mobdeve.s16.group22.medelivery;
-
-import android.widget.Button;
-
-public interface OnItemsClickListener {
-    void onItemClick(Button aButton);
-}
